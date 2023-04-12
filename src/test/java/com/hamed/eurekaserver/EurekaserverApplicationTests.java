b32d3fbec2355e4ad417be39528f2d08c154de5a
Aliasghar Yaghoobzadeh <ha.yaghoobzadeh@gmail.com>
2023-04-12 19:46:29 +0430

    initial commit

---
+++ src/test/java/com/hamed/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamed.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
